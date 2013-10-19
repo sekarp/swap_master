96a9203494d604a5eab3dacb791d1474e3f59cc8
Pradeepa Sekar <pradeepa.sekar@infospace.com>
2013-10-19 08:33:13 -0700

    Adding a test project

---
+++ TestProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers();
+            Console.Read();
+        }
+
+        static void PrintNumbers()
+        {
+            for(int i = 0; i < 10; i++ )
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
